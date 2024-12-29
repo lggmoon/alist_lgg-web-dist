d24c582715a3b52e78b72d1561884dd932fa4d3c
Noah Hsu <i@nn.ci>
2024-12-29 11:12:00 +0000

    upload v1.2.0 dist files

---
+++ dist/assets/index.8777ae9f.js
@@ -0,0 +1,1 @@
+import{h as t,a7 as e,a4 as r}from"./index.30b9a431.js";import n from"./Upload.ea2ba52a.js";import"./index.52412a56.js";const c=()=>t(r,{justifyContent:"center",h:"100vh",get children(){return t(e,{w:"$md",get children(){return t(n,{})}})}});export{c as default};
