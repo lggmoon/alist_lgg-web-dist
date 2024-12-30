9f9a2b877730d2466af4185f1158c793d30892b4
Noah Hsu <i@nn.ci>
2024-12-30 06:23:41 +0000

    upload v1.2.1 dist files

--- dist/assets/Wether.461e2025.js
+++
@@ -1,1 +0,0 @@
-import{a as t,h as s,cl as n}from"./index.30b9a431.js";const a=e=>{const r=t();return s(n,{get colorScheme(){return e.yes?"success":"danger"},get children(){return r(`global.${e.yes?"yes":"no"}`)}})};export{a as W};
